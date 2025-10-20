baed91f9be86619304f995b821d57ad8c5d55734
mahdictf <149879310+mahdictf@users.noreply.github.com>
2025-10-20 11:37:46 +0200

    die gegebene Aufgaben lösen

---
+++ 2025-10-20/aufgabe4-10.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+
+
+
+/*Aufgabe 4-10:
+Datûm: 20.10.2025
+Erstellt von: [Mahdi ]
+*/
+
+
+int main (void){
+
+    int inputZahl;
+
+    printf("Wurzel aus einer Zahlberechnen\n");
+    printf("================================\n\n");
+
+    printf("Bitte eine Zahl eingeben: ");
+    scanf("%d", &inputZahl);
+
+    printf("Die Wurzel aus %d ist %.2f\n", inputZahl, sqrt(inputZahl));
+
+
+
+
+
+    return 0;
+}
---
+++ 2025-10-20/aufgabe4-11.c
@@ -0,0 +1,33 @@
+
+
+#include <stdio.h>
+#include <math.h>
+
+/*
+Aufgabe 4-10:
+Datûm: 20.10.2025
+Erstellt von: [Mahdi ]
+
+ */
+
+int main (void){
+
+    int ersteZahl;
+    int zweiteZahl;
+
+
+    printf("Potenzberechung\n");
+    printf("===============\n\n");
+    printf("bitte geben Sie die Basis ein: ");
+    scanf("%d", &ersteZahl);
+    printf("bitte geben Sie den Exponenten ein: ");
+    scanf("%d", &zweiteZahl);
+
+    double ergebnis = pow(ersteZahl, zweiteZahl);
+    /* Ergebnis auf ganze Zahl runden und als Integer ausgeben, damit keine ".00" mehr erscheint */
+    long long ganz = llround(ergebnis);
+    printf("%d hoch %d ist %lld\n", ersteZahl, zweiteZahl, ganz);
+
+
+    return 0;
+}
