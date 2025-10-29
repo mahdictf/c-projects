1455507d507570d0836d76f45c38abc1dad0fa4b
mahdictf <149879310+mahdictf@users.noreply.github.com>
2025-10-29 15:58:12 +0100

    aufgabe4-12 lösen

--- 2025-10-20/aufgabe4-12.c
+++ 2025-10-20/aufgabe4-12.c
@@ -13,6 +13,29 @@ Aufgabe 4-12:
 
 int main(void){
 
+//deklaration der Variablen
+double v0;
+double alpha;
+double flugweite;
+const double pi = 3.1416;
+
+//eingabeaufforderung
+printf("Bitte geben Sie die Anfangsgeschwindigkeit (in m/s) ein: ");
+scanf("%lf", &v0);
+
+printf("Bitte geben Sie den Abwurfwinkel (in Grad) ein: ");
+scanf("%lf", &alpha);
+
+
+
+flugweite = (v0 * v0 * sin(2 * alpha * (pi / 180))) / 9.81;
+
+//ausgabe der flugweite
+printf("Die berechnete Flugweite beträgt: %.2f Meter\n", flugweite);
+return 0;
+
+
+
     
 
 
