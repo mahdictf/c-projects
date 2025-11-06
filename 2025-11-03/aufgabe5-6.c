beb89c03e0d9353f307b3195d662580a50fd5848
mahdictf <149879310+mahdictf@users.noreply.github.com>
2025-11-06 21:44:58 +0100

    Vorbereitung der  aufgeben von 03.11.25

---
+++ 2025-11-03/aufgabe5-6.c
@@ -0,0 +1,13 @@
+
+
+/*
+autor: mahdi
+datum: 06.11.2025
+Aufgabe 5-5:
+beschreibung: Schreiben Sie ein Programm, welches ein Jahr darauf testet, ob es ein Schaltjahr ist oder nicht.
+
+Hinweis:
+    Ein Jahr ist ein Schaltjahr, wenn es durch 4 teilbar ist,
+    aber nicht durch 100, ausser es ist auch durch 400 teilbar.
+*/
+
---
+++ 2025-11-03/aufgabe5-7.c
@@ -0,0 +1,9 @@
+
+/*
+autor: mahdi
+datum: 06.11.2025
+Aufgabe 5-7:
+Beschreibung: Schreiben Sie ein Programm, welches eine Schulnote in Worten ausdrückt:
+
+*/
+
